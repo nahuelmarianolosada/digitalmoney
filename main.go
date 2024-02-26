6541751f27adf369a6633c0f76438e90cfd6be19
nahuelmarianolosada <yosoyyo.nahuel@gmail.com>
2024-02-26 00:38:19 -0300

    soy el nuevo commit

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ func main() {
 
 func initRouter() *gin.Engine {
 	router := gin.Default()
+	///////////////////////////////////////////// soy del primer commit
 	router.GET("/ping", controllers.Ping)
 	api := router.Group("/api")
 	{
